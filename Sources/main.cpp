9fc9a38020a5db3edb79d67a9f9c7c9337a57fb5
Qingwei Zhang <qzhang.canada@gmail.com>
2025-07-13 21:48:11 -0400

    refactor: expose internal OrderBook and streamline benchmarks
    - Add MatchingEngine::getOrderBook() to allow benchmarks to use default ctor
    - Update benchmark_orders.cpp, benchmark_orders_mt_v2.cpp, and benchmark_latency.cpp to grab internal OrderBook via getter
    - Remove redundant benchmark files (benchmark_orders_v2.cpp, benchmark_orders_v3.cpp, benchmark_latency_mt.cpp, benchmark_orders_mt.cpp)
    - Clean up CMakeLists.txt to only include the three core benchmark targets

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -6,27 +6,4 @@
 #include "../Headers/MatchingEngine.h"
 #include "../Headers/OrderBook.h"
 
-int main() {
-  OrderBook order_book;
-  MatchingEngine engine(order_book);
-
-  // ???? 6 ??????????????
-  order_book.addOrder(Order("buy1", OrderSide::BUY, 105.0, 10));
-  order_book.addOrder(Order("sell1", OrderSide::SELL, 95.0, 10));
-  order_book.addOrder(Order("buy2", OrderSide::BUY, 105.0, 10));
-  order_book.addOrder(Order("sell2", OrderSide::SELL, 95.0, 10));
-  order_book.addOrder(Order("buy3", OrderSide::BUY, 105.0, 10));
-  order_book.addOrder(Order("sell3", OrderSide::SELL, 95.0, 10));
-
-  std::cout << "[INFO] Starting match...\n";
-
-  auto trades = engine.match();
-
-  std::cout << "[RESULT] Trades executed: " << trades.size() << "\n";
-  for (const auto& t : trades) {
-    std::cout << "  - " << t.buyer_id << " buys from " << t.seller_id << " at "
-              << t.price << " qty " << t.quantity << "\n";
-  }
-
-  return 0;
-}
+int main() { return 0; }
